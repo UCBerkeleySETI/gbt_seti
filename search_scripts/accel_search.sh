61707896fe08ef5deb49d47063836c220ec58ef4
Alessio <lessju@gmail.com>
2011-11-29 17:13:44 +0100

    Use mounted remote drive for search

--- search_scripts/accel_search.sh
+++ search_scripts/accel_search.sh
@@ -6,14 +6,37 @@
 . /etc/profile.d/modules.sh
 module load gcc
 
+# Define environmental variables
 export PATH=$PATH:/usr/local/bin:/home/amag0001/tempo2
 export PGPLOT_DIR=/opt/pgplot
 export PRESTO=/home/amag0001/presto
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PRESTO/lib:$PRESTO/lib64:/opt/pgplot
 
-files=(`ls B1839+56_8bit.fil/*.dat`)
+# List all files to be processed
+files=(`ssh master ls $HOME/SETI/Data/B1839+56_8bit.fil/*.dat`)
 numFiles=$((${#files[*]} / 32))
 
-echo "Starting Accel Search: `date`"
-ls ${files[@]:$((($SGE_TASK_ID-1)*$numFiles)):$numFiles} | xargs -n 1 $PRESTO/bin/accelsearch -zmax 0 -harmpolish > SearchOutput.txt
-echo "Finished Accel Search: `date`"
+# Process all file for this job task
+echo "Starting accel search: `date`"
+
+for (( i=0; i<$numFiles; i++))
+do
+    # Get current file name
+    file=${files[$((($SGE_TASK_ID-1)*$numFiles+$i))]}
+    filename=${file##*/}
+
+    # Copy dat and inf file for this file locally
+    scp master:${file%\.dat}* .
+  
+    # Perform acceleration search on copied files
+    $PRESTO/bin/accelsearch -zmax 0 -harmpolish $filename >> SearchOutput.txt
+
+    # Copy cand results to remote store
+    scp *ACCEL* master:$HOME/SETI/Data/B1839+56_8bit.fil/
+
+    # Remove files
+    rm -f ${filename%\.dat}*
+done
+
+echo "Finished accel search: `date`"
+
