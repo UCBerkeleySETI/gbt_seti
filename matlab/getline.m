b2aba1ffb29a96c5e9890d03be93ba9230f28f78
siemion <siemion@berkeley.edu>
2013-09-15 00:06:04 -0700

    getline

---
+++ matlab/getline.m
@@ -0,0 +1,34 @@
+function line = getline(mt, str, stp, width)
+
+
+
+if str==stp
+    line = mt(:,str);
+else
+
+	 length = (size(mt,1)^2 + abs(str-stp)^2)^(1/2);
+	 length = round(length);
+
+	 m = (size(mt,1) - 1)/(stp - str);
+	 b = 0.5 - ((str - 0.5) * m);
+
+	 for i = 1:1:length
+
+	 x = (str - 0.5) + (i-1) * (stp-str)/(length - 1);
+	 y = m * x + b;
+	 x;
+	 y;
+
+	 line(i) = mt(round(y), round(x));
+	 for j=1:1:width
+	
+		 line(i) = line(i) + mt(round(y), min([(round(x) + j) size(mt,2)]) );
+		 line(i) = line(i) + mt(round(y), max([(round(x) - j) 1]) );
+	 end
+
+	 round(y);
+	 round(x);
+
+	 end
+
+end
