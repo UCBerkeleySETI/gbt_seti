9306591c9f4e5fed12093d0258cb4da07f45afac
Andrew Siemion <siemion@blh0.bl.pvt>
2016-12-21 11:01:59 -0500

    added dspsr GUPPIFile substitution here for now

---
+++ src/GUPPIFile.C
@@ -0,0 +1,231 @@
+/***************************************************************************
+ *
+ *   Copyright (C) 2010 by Paul Demorest
+ *   Licensed under the Academic Free License version 2.1
+ *
+ ***************************************************************************/
+#include "dsp/GUPPIFile.h"
+
+#include "Error.h"
+
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#include "fitshead.h"
+#include "fitshead_utils.h"
+
+using namespace std;
+
+dsp::GUPPIFile::GUPPIFile (const char* filename)
+  : GUPPIBlockFile ("GUPPI")
+{
+}
+
+dsp::GUPPIFile::~GUPPIFile ( )
+{
+  // Free things.
+  if (hdr != NULL) free(hdr);
+  if (dat != NULL) free(dat);
+}
+
+// Read header starting from current spot in file.  Puts header info
+// into hdr buf, and returns total number of keys found.  If no
+// "END" is found, frees hdr and return 0.
+// TODO could rewrite this using the fitshead.h stuff now, but 
+// this already exists..
+int get_header(int fd, char **hdr) {
+  const size_t cs = 80;
+  char card[cs+1];
+  char end[cs+1];
+  card[cs] = '\0';
+  end[cs] = '\0';
+  memset(end, ' ', cs);
+  strncpy(end, "END", 3);
+  const int max_cards = 2304;
+  int count=0;
+  bool got_end=false;
+  int direct_io=0;
+
+  while (count<max_cards && !got_end) {
+
+    // Read next card
+    unsigned rv = read(fd, card, cs);
+    if (rv<0) // Error
+      throw Error (FailedSys, "dsp::GUPPIFile::get_header",
+          "read() failed");
+    else if (rv!=cs) // Probably EOF
+      break;
+
+    // Check for END
+    if (strncmp(card,end,cs)==0)
+      got_end = true;
+
+    count++;
+
+    // Print line for debug
+    // cerr << card << endl;
+
+    // Copy into hdr buf
+    *hdr = (char *)realloc(*hdr, count*cs + 1);
+    strncpy(&(*hdr)[(count-1)*cs], card, cs);
+    (*hdr)[count*cs] = '\0';
+  }
+
+  if (got_end==false) {
+    free(*hdr);
+    *hdr = NULL;
+    return 0;
+  }
+
+  hget(*hdr, "DIRECTIO", &direct_io);
+  if (direct_io) {
+    lseek(fd, ((1<<30) - (cs*count)) % 512, SEEK_CUR);
+  }
+
+  return count;
+}
+
+bool dsp::GUPPIFile::is_valid (const char* filename) const
+{
+
+  int fd = ::open(filename, O_RDONLY);
+  if (fd < 0) 
+    throw Error (FailedSys, "dsp::GUPPIFile::is_valid",
+        "fopen(%s) failed", filename);
+
+  // Try reading header
+  char *hdr_tmp=NULL;
+  int nkeys = get_header(fd, &hdr_tmp);
+  ::close(fd);
+  if (nkeys<=0) {
+    if (verbose)
+      cerr << "dsp::GUPPIFile couldn't parse header." << endl;
+    return false;
+  }
+
+  // If header read ok, check for some required keywords
+  // that don't appear in psrfits.
+  int rv, itmp;
+
+  rv = hget(hdr_tmp, "BLOCSIZE", &itmp);
+  if (rv<0) {
+    if (verbose)
+      cerr << "dsp::GUPPIFile coulnd't find BLOCSIZE keyword." << endl;
+    return false;
+  }
+
+  rv = hget(hdr_tmp, "PKTIDX", &itmp);
+  if (rv<0) {
+    if (verbose)
+      cerr << "dsp::GUPPIFile coulnd't find PKTIDX keyword." << endl;
+    return false;
+  }
+
+  // Everything passed, it's probably a GUPPI file.
+  return true;
+}
+
+void dsp::GUPPIFile::open_file (const char* filename)
+{
+
+  // Open file
+  fd = ::open(filename, O_RDONLY);
+  if (fd < 0)
+    throw Error (FailedSys, "dsp::GUPPIFile::open_file",
+        "open(%s) failed", filename);
+
+  // Read in header
+  int hdr_keys = get_header(fd, &hdr);
+  if (hdr_keys<=0) 
+    throw Error (InvalidState, "dsp::GUPPIFile::open_file",
+        "Error parsing header.");
+
+  // Parse header into info struct
+  parse_header();
+
+  int hdrsize = 80*hdr_keys;
+  int direct_io = 0;
+  hget(hdr, "DIRECTIO", &direct_io);
+  if (direct_io) {
+    // Round up to next multiple of 512
+    hdrsize = ((hdrsize + 511) / 512) * 512;
+  }
+
+  // Figure out total size
+  // dfac thing accounts for some weirdness in definition of 
+  // OVERLAP for real-sampled TDOM data..
+  struct stat buf;
+  int rv = fstat(fd, &buf);
+  if (rv < 0)
+    throw Error (FailedSys, "dsp::GUPPIFile::open_file",
+        "fstat(%s) failed", filename);
+  uint64_t full_block_size = hdrsize + blocsize;
+  uint64_t nblocks = buf.st_size / full_block_size;
+  unsigned int dfac = get_info()->get_ndim()==1 ? 2 : 1;
+  get_info()->set_ndat( get_info()->get_nsamples(nblocks*blocsize) - dfac*overlap*nblocks );
+
+  // Rewind and load full first block with header
+  seek_bytes(0);
+  
+}
+
+int dsp::GUPPIFile::load_next_block ()
+{
+  // Assume we're at the right spot in the file, is start of
+  // next header.  For now, we will ignore header params
+  // and assume nothing is changing or skipped.
+
+  // This could also be 0 at EOF.
+  int nkeys = get_header(fd, &hdr);
+  if (nkeys==0) 
+    return 0;
+    //throw Error (InvalidState, "dsp::GUPPIFile::load_next_block",
+    //    "Error loading next header");
+
+  // Make sure memory is allocated
+  if (dat==NULL)
+    dat = (unsigned char *)malloc(blocsize);
+
+  // Read the data
+  size_t nbytes = read(fd, dat, blocsize);
+  if (nbytes < 0)
+    throw Error (FailedSys, "dsp::GUPPIFile::load_next_block", "read");
+
+  if (nbytes != blocsize) return 0;
+
+  return 1;
+}
+
+int64_t dsp::GUPPIFile::seek_bytes (uint64_t bytes)
+{
+  if (bytes==0)
+  {
+    int rv = lseek(fd, 0, SEEK_SET);
+    if (rv<0) 
+      throw Error (FailedSys, "dsp::GUPPIFile::seek_bytes",
+          "lseek(0) failed");
+    load_next_block();
+    
+    current_block_byte = 0;
+    return 0;
+  }
+  else
+    throw Error (InvalidState, "dsp::GUPPIFile::seek_bytes",
+        "seek(%lld) not implemented yet", bytes);
+}
+
+void dsp::GUPPIFile::close ()
+{
+  // Free up the memory buffer if file has been closed
+  if (dat != NULL) 
+  {
+    free(dat);
+    dat = NULL;
+  }
+  // Call the standard close routine
+  File::close();
+}
